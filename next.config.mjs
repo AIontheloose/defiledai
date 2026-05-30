f829f5a2492cd6c63c4333cd6000bf40c510d9d1
AIontheloose <AIgotanemail@proton.me>
2026-05-30 10:36:43 +1000

    Disable webpack cache for Cloudflare Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,20 @@
 import createMDX from "@next/mdx";
 
 const withMDX = createMDX({
-  extension: /\\.mdx?$/,
+  extension: /\.mdx?$/,
 });
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "md", "mdx"],
+
+  webpack: (config, { dev }) => {
+    if (!dev) {
+      config.cache = false;
+    }
+
+    return config;
+  },
 };
 
 export default withMDX(nextConfig);
